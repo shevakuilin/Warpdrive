a6df7278126b242775155bd52bb03bfb8e9a76e0
shevakuilin <shevakuilin@gmail.com>
2018-12-20 12:31:32 +0800

    CREAT SKWebsiteInfo

---
+++ Warpdrive/Warpdrive/Model/SKWebsiteInfo.swift
@@ -0,0 +1,15 @@
+//
+//  SKWebsiteInfo.swift
+//  Warpdrive
+//
+//  Created by ShevaKuilin on 2018/12/20.
+//  Copyright © 2018 ShevaKuilin. All rights reserved.
+//
+
+import Cocoa
+
+class SKWebsiteInfo: NSObject {
+    public var website: String?         /// 网站url
+    public var webName: String?         /// 站点名称
+    public var websiteIconUrl: String?  /// 网站icon
+}
